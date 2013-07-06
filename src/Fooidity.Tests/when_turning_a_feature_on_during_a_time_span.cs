3c7920fe334b3016124ca711a5a44ff0795101ff
dnak <dnak@github.com>
2013-07-05 22:16:23 -0700

    enable a fooid during a time span

---
+++ src/Fooidity.Tests/when_turning_a_feature_on_during_a_time_span.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Fooidity.Tests
+{
+    [TestFixture]
+    public class when_turning_a_feature_on_during_a_time_span
+    {
+        [Test]
+        public void then_the_feature_is_disabled_prior_to_the_start()
+        {
+            var foo = new EnabledDuring<TestFeature>(DateTimeOffset.MaxValue.AddSeconds(-1), 
+                                                     DateTimeOffset.MaxValue);
+
+            Assert.IsFalse(foo.Enabled);
+        }
+
+        [Test]
+        public void then_the_feature_is_disabled_after_the_end()
+        {
+            var foo = new EnabledDuring<TestFeature>(DateTimeOffset.MinValue,
+                                                     DateTimeOffset.MinValue.AddSeconds(1));
+
+            Assert.IsFalse(foo.Enabled);
+        }
+
+        [Test]
+        public void then_the_feature_is_enabled_during_the_time_span()
+        {
+            var foo = new EnabledDuring<TestFeature>(DateTimeOffset.MinValue,
+                                                     DateTimeOffset.Now.AddMinutes(1));
+
+            Assert.IsTrue(foo.Enabled);
+        }
+    }
+}
---
+++ src/Fooidity/EnabledDuring.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fooidity
+{
+    public class EnabledDuring<TFoo> :
+        FooId<TFoo>
+        where TFoo : FooId
+    {
+        private readonly DateTimeOffset _startTime;
+        private readonly DateTimeOffset _endTime;
+
+        public EnabledDuring(DateTimeOffset startTime,
+                             DateTimeOffset endTime)
+        {
+            _startTime = startTime;
+            _endTime = endTime;
+        }
+
+        public bool Enabled
+        {
+            get
+            {
+                var now = DateTimeOffset.Now;
+                return now >= _startTime && now < _endTime;
+            }
+        }
+    }
+}
