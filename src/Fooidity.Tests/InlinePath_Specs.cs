2c3b37087d5c506c7690c624c6cb462632396270
dnak <dnak@github.com>
2013-07-05 21:06:03 -0700

    clarifying inline usage for myself

--- src/Fooidity.Tests/InlinePath_Specs.cs
+++ src/Fooidity.Tests/InlinePath_Specs.cs
@@ -7,29 +7,10 @@
     public class Using_a_fooid_inline
     {
         [Test]
-        public void Should_provide_a_clear_syntax_for_method_selection()
+        public void Should_allow_for_enabled_features()
         {
             FooId<TestFeature> fooId = FooIds.Enabled<TestFeature>();
-
-            fooId.If(() =>
-                {
-                    // run if it is enabled
-                });
-
-            fooId.Unless(() =>
-                {
-                    // run if disabled
-                });
-
-            fooId.If(() =>
-                {
-                    /* enabled */
-                }, () =>
-                    {
-                        /* disabled */
-                    });
-
-
+            
             var result = fooId.Iff(() => 27, () => 42);
             Assert.AreEqual(27, result);
 
@@ -37,6 +18,31 @@
             Assert.AreEqual(27, value);
         }
 
+        [Test]
+        public void Should_allow_for_disabled_features()
+        {
+            FooId<TestFeature> fooId = FooIds.Disabled<TestFeature>();
+
+            var result = fooId.Iff(() => 27, () => 42);
+            Assert.AreEqual(42, result);
+
+            var value = fooId.Iff((a, b) => a, (a, b) => b, 27, 42);
+            Assert.AreEqual(42, value);
+        }
+
+        [Test]
+        public void Should_allow_for_changing_feature_state()
+        {
+            FooId<TestFeature> fooId = FooIds.Disabled<TestFeature>();
+
+            var result = fooId.Iff(() => 27, () => 42);
+            Assert.AreEqual(42, result);
+
+            fooId = FooIds.Enabled<TestFeature>();
+
+            var value = fooId.Iff(() => 27, () => 42);
+            Assert.AreEqual(27, value);
+        }
 
         class TestFeature :
             FooId
